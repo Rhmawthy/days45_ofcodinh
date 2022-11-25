26ea5870c2288825936980fb3cf0b75d39d82768
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-25 19:59:47 +0800

    Add files via upload

---
+++ days45_menukar 2 nilai variabel.py
@@ -0,0 +1,12 @@
+# program inputan menukar 2 buah nilai variabel
+
+kata1 = input("kata pertama :")
+kata2 = input("kata kedua :")
+
+asli= kata1,kata2
+menukar = kata2, kata1
+
+
+print("nilai sebelum di tukar",asli)
+
+print("nilai setelah di tukar",menukar)
